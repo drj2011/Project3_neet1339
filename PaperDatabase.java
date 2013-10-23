089a2b9a8308d1dac754abbce81489d2b8ff6f76
drj2011 <drj2011@yahoo.com>
2013-10-22 21:15:33 -0500

    Test

---
+++ PaperDatabase.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+
+
+/**
+ *  <P>
+ * A class used to create a PaperDatabase object.  It can construct a PaperDatabase
+ * object, sort the Paper objects of the database, search the Paper objects of the
+ * database and output the PaperDatabase as a .txt file to a specified location.
+ * </P>
+ *
+ */
+/**
+ * @author 
+ *
+ */
+public class PaperDatabase {
+
+	private ArrayList<Paper> paperList = new ArrayList<Paper>();
+
+
+	/**
+	 * <P>
+	 * A constructor for an object type PaperDatabase.
+	 * <P> 
+	 */
+	public PaperDatabase() {
+		
+	}
+	
+	/**
+	 * <P>
+	 * Used to create the paper database that is used throughout the program.
+	 * </P>
+	 * 
+	 * @throws IOException
+	 */
+	public void fillDirectory() throws IOException{
+	
+	}
+	/**
+	 * <P>
+	 * Sorts the PaperDatabase how the user chooses.
+	 * <P> 
+	 * 
+	 * @param userInput - Method to sort the database.
+	 */
+	public void sortRouting(int userInput) {
+
+		
+	}
+
+
+	/**
+	 * <P>
+	 * Searches the PaperDatabase for the paper with the title given.
+	 * <P> 
+	 * 
+	 * @param userInput - Name of the paper to search for.
+	 */
+	public void search(String userInput) {
+		
+	}
+
+	public void binarySearch(String searchType, String key){
+		
+	}
+
+	/** 
+	 * <P>
+	 * Outputs PaperDatabase to text file.
+	 * <P> 
+	 */
+	public void output(){
+		
+	}
+	
+	public void fileWriter(String fileName) throws IOException{
+		
+	}
+
+
+
+}
